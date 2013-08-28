ba7298fdb376aa66a5cde5b4d950b112b563466a
jhermann <jh@web.de>
2013-08-28 17:58:20 +0200

    test script + config for webdav upload

---
+++ dput-webdav/test.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+set -e
+
+fail() { # fail with error message and exit code 1
+    echo >&2 "ERROR:" "$@"
+    exit 1
+}
+
+dput_test() {
+    dput --config dput.cf --debug --force --unchecked "$@"
+}
+
+
+echo
+echo "*** Python unit tests **"
+python -m webdav
+
+if test $(ls -1 build/*.changes | wc -l) -ne 1; then
+    echo
+    echo "*** Preparing test package **"
+    rm -rf build/artifactory-debian-webdav-test* build/deb 2>/dev/null || :
+    mkdir -p build/deb
+    pushd build/deb >/dev/null
+    echo | dh_make -s --indep --createorig -p artifactory-debian-webdav-test_1.0
+    dpkg-buildpackage -uc -us 
+    popd >/dev/null
+fi
+
+echo
+echo "*** Printing effective test config **"
+dput_test --print
+
+echo
+echo "*** dput integration test - simulating an upload **"
+test -r "/usr/share/dput/webdav.py" || fail "You need to install webdav.py to /usr/share/dput"
+dput_test 'artifactory-debian:integration-test;repo=foo+bar' build/*.changes | tee build/dput.log
+grep ".repo.: .foo bar." build/dput.log >/dev/null || fail "Host argument passing doesn't work"
+set +x
+
+echo
+echo "** ALL OK **"
+
